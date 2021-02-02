50d62535c4a275bdde0419a5bf020b3306d762f4
mdmahbubulalam <alam.diu13@gmail.com>
2021-02-02 17:14:19 +0600

    double equal vs triple equal , implicit conversion

---
+++ doubleVsTripleEquals.js
@@ -0,0 +1,14 @@
+//== means only value check korbe
+//=== means value and type soho check korbe
+//so nicher condition false
+const first = 1;
+//const second = "2";
+const second = true; //ekhane == er jonno false ashbe, karon true means 1 bujhay and false means 0 bujhay. 
+//jodi const first=1 hoto, tokhon true ashto. === equal dile false hobe, karon onst first = 1 hochce number
+//type and const second = true hocche boolean type. duitar type ek na so false.
+
+if(first === second){
+    console.log('Condition is true');
+}else{
+    console.log('Condition is false');
+}
